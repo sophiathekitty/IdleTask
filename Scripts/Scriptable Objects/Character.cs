08823e4f1521fb487d284c50b91f429210c2b647
sophia daniels <sophiathekitty@users.noreply.github.com>
2018-07-01 04:10:00 -0600

    adding some character stuffs

---
+++ Scripts/Scriptable Objects/Character.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Characters/Character")]
+public class Character : ScriptableObject
+{
+    public string full_name;
+    public string short_name;
+    public IntVariable level;
+    public AttributeVariable health;
+    public AttributeVariable energy;
+    public AttributeVariable experience;
+    public FloatVariable strength;
+    public FloatVariable will;
+    public FloatVariable stamina;
+    public FloatVariable perception;
+    public FloatVariable speed;
+}
