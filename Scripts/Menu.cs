2d3024066e3df7cd3fcb5585e10c4bdcc1aae69e
sophia daniels <sophiathekitty@users.noreply.github.com>
2018-08-01 18:18:34 -0600

    added menu

---
+++ Scripts/Menu.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour {
+    public SaveObject saveObject;
+    public void ClearData()
+    {
+        saveObject.clearSaveData();
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
